175367235eb2a35f091013c1e56f8881581ec0aa
GitMuhammadS <mdshahbazengg@gmail.com>
2024-08-24 03:29:58 +0530

    my 1st commit at 3:29

---
+++ src/main/java/com/shahbaz/Rtp1ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.shahbaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rtp1ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rtp1ToDoAppApplication.class, args);
+	}
+
+}
